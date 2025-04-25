51430575da6a9f11990021deb8bd13574c0b1e0b
yurii.tomko <yurii.tomko@valtech.com>
2025-04-25 12:51:25 +0300

    Created contact component with model

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/YuriiContactModel.java
@@ -0,0 +1,60 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = {Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class YuriiContactModel  {
+    @ValueMapValue
+    private String title;
+    @ValueMapValue
+    private String firstName;
+    @ValueMapValue
+    private String lastName;
+    @ValueMapValue
+    private String phoneNumber;
+    @ValueMapValue
+    private String email;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
