9c17746e97f02502e743664740e3c777857ecf4d
yurii.tomko <yurii.tomko@valtech.com>
2025-05-05 12:55:04 +0300

    UI component changes

--- ui.frontend/src/main/webpack/site/main.js
+++ ui.frontend/src/main/webpack/site/main.js
@@ -2,6 +2,7 @@
 import './main.scss';
 
 // Typescript/Javascript
+
 import './util.js';
 import './scroll-indicator';
 import '../components/languagenavigation/languagenavigation.js';
@@ -9,4 +10,5 @@ import '../components/layout-container/modal.js';
 import '../components/navigation/navigation.js';
 import '../components/form/sign-in-buttons/sign-in-buttons.js';
 import '../components/form/sign-in-form/sign-in-form.js';
+import '../components/yuriicomponent/contats.js';
 import '../components/contentfragment/js/contributor.js';
