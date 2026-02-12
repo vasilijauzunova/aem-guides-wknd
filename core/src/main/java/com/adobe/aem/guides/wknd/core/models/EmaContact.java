fd3f2355cd2fe53a9c3a2a073d8d234608704a5d
zdraveska <zdraveskaema@gmail.com>
2026-02-12 02:36:35 +0100

    part1: created a contact component

---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/EmaContact.java
@@ -0,0 +1,19 @@
+package com.adobe.aem.guides.wknd.core.models;
+
+public interface EmaContact {
+
+	String getTitle();
+
+	String getFirstName();
+
+	String getLastName();
+
+	String getFullName();
+
+	String getPhoneNumber();
+
+	String getEmailAddress();
+
+	boolean isValidEmail();
+
+}
---
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/impl/EmaContactImpl.java
@@ -0,0 +1,86 @@
+package com.adobe.aem.guides.wknd.core.models.impl;
+
+import com.adobe.aem.guides.wknd.core.models.EmaContact;
+import java.util.regex.Pattern;
+import javax.annotation.PostConstruct;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+@Model(adaptables = {SlingHttpServletRequest.class, Resource.class},
+		adapters = EmaContact.class,
+		resourceType = "wknd/components/ema-contact",
+		defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class EmaContactImpl implements EmaContact {
+
+	private static final Pattern EMAIL_PATTERN = Pattern.compile(
+			"^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+	);
+
+	@ValueMapValue
+	private String title;
+	@ValueMapValue
+	private String firstName;
+	@ValueMapValue
+	private String lastName;
+	@ValueMapValue
+	private String emailAddress;
+	@ValueMapValue
+	private String phoneNumber;
+
+	private boolean isEmailValid;
+
+	@PostConstruct
+	protected void init() {
+		isEmailValid = StringUtils.isNotBlank(emailAddress) &&
+		               EMAIL_PATTERN.matcher(emailAddress.trim()).matches();
+	}
+
+	@Override
+	public String getTitle() {
+		return StringUtils.defaultString(title);
+	}
+
+	@Override
+	public String getFirstName() {
+		return StringUtils.defaultString(firstName);
+	}
+
+	@Override
+	public String getLastName() {
+		return StringUtils.defaultString(lastName);
+	}
+
+	@Override
+	public String getFullName() {
+		StringBuilder fullName = new StringBuilder();
+
+		if (StringUtils.isNotBlank(firstName)) {
+			fullName.append(firstName).append(" ");
+		}
+		if (StringUtils.isNotBlank(lastName)) {
+			fullName.append(lastName);
+		}
+
+		return fullName.toString().trim();
+	}
+
+	@Override
+	public String getPhoneNumber() {
+		return StringUtils.defaultString(phoneNumber).replaceAll("\\s+", "");
+	}
+
+	@Override
+	public String getEmailAddress() {
+		return StringUtils.defaultString(emailAddress).trim();
+	}
+
+	@Override
+	public boolean isValidEmail() {
+		return isEmailValid;
+	}
+}
+
